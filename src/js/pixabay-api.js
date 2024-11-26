8d42f2b00464f39c5d747e2eda1982b03f9fcfad
Dorian Devua <Dorian.DevUa@gmail.com>
2024-11-26 20:52:47 +0400

    add private properties

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,28 +2,46 @@ const API_KEY = '43520057-d4110ce2722b475a1deefaa82';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export default class PixabayApiService {
+  #searchQuery;
+  #galleryPage;
+
   constructor() {
-    this.searchQuery = '';
-    this.page = 1;
+    this.#searchQuery = '';
+    this.#galleryPage = 1;
+  }
+
+  get query() {
+    return this.#searchQuery;
+  }
+
+  set query(newQuery) {
+    this.#searchQuery = newQuery;
+  }
+
+  incrementPage() {
+    this.#galleryPage += 1;
+  }
+
+  resetPage() {
+    this.#galleryPage = 1;
   }
 
   fetchImages() {
     const searchParams = new URLSearchParams({
       key: API_KEY,
-      lang: 'en',
+      q: this.#searchQuery,
+      page: this.#galleryPage,
+      per_page: 5,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      q: this.searchQuery,
-      page: this.page,
     });
-
     const url = `${BASE_URL}?${searchParams}`;
 
     return fetch(url)
       .then(resp => {
         if (!resp.ok) {
-          throw new Error(resp.status);
+          throw new Error(`HTTP error: ${resp.status}`);
         }
 
         return resp.json();
@@ -32,21 +50,5 @@ export default class PixabayApiService {
         this.incrementPage();
         return hits;
       });
-  }
-
-  incrementPage() {
-    this.page += 1;
-  }
-
-  resetPage() {
-    this.page = 1;
-  }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
   }
 }
