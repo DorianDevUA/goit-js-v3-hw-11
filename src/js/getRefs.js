8a1df11df28976da71ee43bdbeabc55b2df174c1
Dorian Devua <Dorian.DevUa@gmail.com>
2024-12-07 22:19:19 +0400

    del ref loadMoreBtn

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -2,7 +2,6 @@ export default function getRefs() {
   return {
     searchForm: document.querySelector('.js-search-form'),
     imageGallery: document.querySelector('.js-gallery'),
-    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
     loader: document.querySelector('.js-loader'),
   };
 }
