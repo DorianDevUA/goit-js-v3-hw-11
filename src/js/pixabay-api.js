d7464908c79321a621ae171c9ceab715a6fc6172
Dorian Devua <Dorian.DevUa@gmail.com>
2025-02-01 13:52:42 +0400

    add Pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,46 +2,46 @@ const API_KEY = '43520057-d4110ce2722b475a1deefaa82';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export default class PixabayApiService {
-  #searchQuery;
-  #galleryPage;
+  #query;
+  #page;
 
   constructor() {
-    this.#searchQuery = '';
-    this.#galleryPage = 1;
-    this.perPage = 10;
+    this.#query = '';
+    this.#page = 1;
     this.isLastPage = false;
+    this.limit = 5;
   }
 
-  get query() {
-    return this.#searchQuery;
+  get searchQuery() {
+    return this.#query;
   }
 
-  set query(newQuery) {
-    this.#searchQuery = newQuery;
+  set searchQuery(newQuery) {
+    this.#query = newQuery.trim().toLowerCase();
   }
 
-  get page() {
-    return this.#galleryPage;
+  get galleryPage() {
+    return this.#page;
   }
 
-  set page(newPage) {
-    this.#galleryPage = newPage;
+  set galleryPage(newPage) {
+    this.#page = newPage;
   }
 
   incrementPage() {
-    this.#galleryPage += 1;
+    this.#page += 1;
   }
 
   resetPage() {
-    this.#galleryPage = 1;
+    this.#page = 1;
   }
 
   fetchImages() {
     const searchParams = new URLSearchParams({
       key: API_KEY,
-      q: this.#searchQuery,
-      page: this.#galleryPage,
-      per_page: this.perPage,
+      q: this.#query,
+      page: this.#page,
+      per_page: this.limit,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
@@ -56,16 +56,16 @@ export default class PixabayApiService {
 
         return resp.json();
       })
-      .then(data => {
-        this.incrementPage();
-        const result = this.#galleryPage * this.perPage;
-        if (result > data.totalHits) {
+      .then(({ hits, totalHits }) => {
+        const total = this.#page * this.limit;
+
+        if (total >= totalHits) {
           this.isLastPage = true;
-          console.log('This is last page!');
-          console.log('isLastPage:', this.isLastPage);
         }
 
-        return data;
+        this.incrementPage();
+
+        return hits;
       });
   }
 }
